69ce5fde0d60ea098647fbe9b723241b0a8112cb
ktlwik <nurzhan.yergozhin@gmail.com>
2016-03-31 19:22:15 +0800

    updated DB, half way calc.php
    updated DB, half way calc.php

--- calc.php
+++ calc.php
@@ -10,27 +10,47 @@
    		return 0; // I need to check whether my course clash with the selected times
    }
 
-   function clash ($i, $j) {
-   	return 0;	// checks if 2 courses clash or not
+   function check ($dayI, $dayJ, $startTimeI, $startTimeJ, $endTimeI, $endTimeJ) {
+     if ($dayI != $dayJ) return 0;
+     if (($endTimeI < $startTimeJ) || ($endTimeJ < $startTimeI)) return 0;
+     return 1; // checks if 2 courses clash or not
    }
+  
 
-   function solve ($index) {
+   function clash ($i, $j) {
+      global $day, $startTime, $endTime;
+      for ($ind1 = 0; $ind1 < count ($day[$i]); ++$ind1) {
+        for ($ind2 = 0; $ind2 < count ($day[$j]); ++$ind2)
+          if (check ($day[$i][$ind1], $day[$j][$ind2], $startTime[$i][$ind1], $startTime[$j][$ind2], $endTime[$i][$ind1], $endTime[$j][$ind2]) == 1) {
+            return 1;
+          }
+      }
+      return 0;
+   }
+    
+    function solve ($index) {
+      global $len, $tables, $ID, $unique_courses, $left, $sz;
+     // print ($index);
    		if ($index > $len) {
    			for ($i = 1; $i <= $len; ++$i) {
-   				for ($j = $i + 1; $j <= $len; ++j) {
-   					if (clash ($ID[$i], $ID[$j]) == 1) return;
+   				for ($j = $i + 1; $j <= $len; ++$j) {
+    				if (clash ($ID[$i], $ID[$j]) == 1) return;
    				}
    			}
-   			for ($i = 1; $i <= $len; ++$i) {
+        print_r ($ID);
+        print "<br>";
+   			/*for ($i = 1; $i <= $len; ++$i) {
    				if (is_collapse ($ID[$i]) == 1) return;
-   			}
+   			}*/
    			// if it succeed until this stage then this is good timetable 
    			array_push($tables, $ID); // all the available timetable ids will be stored in tables
+        return ;
    		}
    		$course = $unique_courses[$index];
+    //  print $left[$course]."<br>". $sz[$course];
    		for ($i = $left[$course]; $i <= $left[$course] + $sz[$course] - 1; ++$i) {
    			$ID[$index] = $i;// assigning indexes
-   			solve (index + 1);
+   			solve ($index + 1, $len);
    		}
 
    }
@@ -49,15 +69,22 @@
    		 $ClassID[$n] = $arg['classID'];  
    		 $arg = mysql_fetch_array($res);  			
   }
-  for ($i = 1; $i <= n; ++$i) {
+  for ($i = 1; $i <= $n; ++$i) {
   	$classID = $ClassID[$i];
   	$courseID = $CourseID[$i];
   	$courses[$i] = $courseID;
   	$res = mysql_query("SELECT * FROM `classes` WHERE `id` = '$classID' 
 		                                        AND `courseID` = '$courseID'");
   	if ($res) $arg = mysql_fetch_array($res);
-  	$startTime[$i] = $arg['startTime'];
-  	$endTime[$i] = $arg['endTime'];
+      $startTime[$i] = array();
+      $endTime[$i] = array();
+      $day[$i] = array();
+  	while(!empty($arg['courseID'])) {
+      array_push($startTime[$i], $arg['startTime']);
+   	  array_push($endTime[$i], $arg['endTime']);
+      array_push($day[$i],$arg['day']);
+      $arg = mysql_fetch_array($res);
+    }
   }
 
   for ($i = 1; $i <= $n; ++$i) {
@@ -67,21 +94,26 @@
                 swap ($classID[$i], $classID[$j]);
                 swap ($startTime[$i], $startTime[$j]);
                 swap ($endTime[$i], $endTime[$j]);
+                swap ($day[$i], $day[$j]);
             }
         }
     }
     
-    $len = n; // length of all uniques courses
+    $len = $n; // length of all uniques courses
     $unique_courses = array_unique($courses);
     $len = count ($unique_courses);
-
+    $sz = array();
+    $left = array();
     
- 	for ($i = 1; $i <= n; ++$i) {
- 		if (empty($left[$CourseID[$i]])) { // finding left index of specific course ID
+ 	for ($i = 1; $i <= $n; ++$i) {
+   if (empty($left[$CourseID[$i]])) { // finding left index of specific course ID
  			$left[$CourseID[$i]] = $i;
  		}
- 		$sz[$CourseID[$i]] ++;
+    if (empty ($sz[$CourseID[$i]])) $sz[$CourseID[$i]] = 1;
+ 		else $sz[$CourseID[$i]] ++;
  	}
-    solve (1);
-    
- ?>
+  $tables = array();
+  solve (1);
+//
+  print_r ($tables);  
+  ?>
---
+++ work.php
@@ -0,0 +1,89 @@
+<?php
+    function is_type($s) {
+    	if ($s == "LEC/STUDIO" || $s == "SEM" || $s == "TUT" || $s == "LAB") return 1;
+    	return 0;
+    }
+    function from_numbers($s) {
+
+    	if (strlen($s) != 5) return 0;
+    	for ($i = 0; $i < strlen ($s); ++$i)
+    		if ($s[$i] < '0' || $s[$i] > '9') return 0;
+    	return 1;
+    }
+
+	include("bd.php");
+	$dir    = './courses';
+	$files = scandir($dir, 0);
+	for ($j = 2; $j < count($files); ++$j) {
+		$len = 0;
+		$myfile = fopen("./courses/".$files[$j], "r") or die("Unable to open file!");
+    	unset ($str);
+		$str = array();
+		while(!feof($myfile)) {
+ 			// echo fgets($myfile) . "<br>";
+ 			 $cur = fgets($myfile);
+ 		 	$str[$len] = $cur;
+ 		 	++$len;
+ 		}
+ 		$str = array_values(array_filter($str, "trim"));
+ 		$len = count ($str);
+ 		for ($i = 1; $i < $len; ++$i) 
+ 			$str[$i] = trim ($str[$i]);
+ 		echo "Suceess!". "<br>";
+ 		for ($i = 1; $i < $len; ++$i) {
+ 			if ($str[$i] == "INDEX" && $i > 3) {
+ 				$courseID = $str[$i - 3];
+ 				$courseName = $str[$i - 2];
+ 				$AU = $str[$i - 1];	
+ 				$res = mysql_query("INSERT INTO `course` (`courseID`, `courseName`, `AU`) VALUES ('$courseID', '$courseName', '$AU')");
+ 
+ 				//$courseID, $courseName, $AU -> update courses
+ 				/*echo "courseID: " . $courseID . "<br>" . "course Name: " . $courseName . "<br>" . "AU: " . $AU ."<br>";
+ 				echo "---------------<br>";*/
+ 			}
+ 			else if (from_numbers($str[$i ]) == 1) {
+ 				$classID = $str[$i];
+ 				//echo "***NEW CLASS ID***" . $classID. "<br>";
+ 			}
+ 			else if (is_type($str[$i]) == 1) {
+ 			//	print ("Cool");
+ 		    	$type = $group = $day = $time = $venue = $remark = "NONE";
+ 				$type = $str[$i];
+ 				++$i;
+ 				if ($i >= $len || is_type ($str[$i]) == 1) goto finish;
+ 				$group = $str[$i];
+ 				++$i;
+ 				if ($i >= $len || is_type ($str[$i]) == 1) goto finish; 
+ 				$day = $str[$i];
+ 				++$i;
+ 				if ($i >= $len || is_type ($str[$i]) == 1) goto finish; 
+ 				$time = $str[$i];
+ 				++$i;
+ 				if ($i >= $len || is_type ($str[$i]) == 1) goto finish; 
+ 				$venue = $str[$i]; 
+ 				++$i;
+ 				if ($i >= $len || from_numbers($str[$i])) {
+ 					--$i;
+ 					goto finish;
+ 				}
+ 		    	if ($i >= $len || is_type ($str[$i]) == 1) goto finish; 
+ 				$remark = $str[$i];
+
+ 				finish :
+ 				if ($time == "NONE") {
+ 					$startTime = "NONE";
+ 					$endTIme = "NONE";
+ 				}
+ 				else {
+ 					$startTime = $time[0].$time[1].$time[2].$time[3];  	
+ 					$endTime = $time[5].$time[6].$time[7].$time[8];  
+ 			
+ 				}
+				/*$res = mysql_query("INSERT INTO `classes` (`id`, `name`, `startTime`, `endTime`, `type`, `cgroup`, `day`, `venue`, `courseID`, `remark`) VALUES ('$classID', '$courseName', '$startTime', '$endTime', '$type', '$group', '$day', '$venue', '$courseID', '$remark')");*/
+ 				//'$classID', '$courseName', '$startTime', '$endTime', '$type', '$group', '$day', '$venue', '$courseID', '$remark' -> update classes
+ 				/*echo "Course ID: " . $courseID . "<br>" . "Type : " . $type . "<br>". "Group: ". $group. "<br>". "Day: ". $day. "<br>". "startTime: ". $startTime. "<br>". "endTime: ". $endTime. "<br>". "Venue: ". $venue. "<br>". "Remark: ". $remark. "<br>" . "^^^^^^^^^^^^^^^^^^^^^". "<br>";*/
+ 			}
+ 		}
+ 	}
+ 		
+?>
