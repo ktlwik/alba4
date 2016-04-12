0242a0b63addd41a512b787188f232c0e0983be3
ktlwik <nurzhan.yergozhin@gmail.com>
2016-04-12 17:16:40 +0800

    Updated Calc with Settings
    Updated Calc with Settings

--- calc.php
+++ calc.php
@@ -7,16 +7,25 @@
     }
      
    function is_collapse($i) {
-   		return 0; // I need to check whether my course clash with the selected times
+      global $day, $startTime, $endTime;
+      for ($ind = 0; $ind < count ($day[$i]); ++$ind) {
+        $Day = $day[$i][$ind];
+        $res = mysql_query("SELECT * FROM `Settings` WHERE `day` = '$Day'");
+        if ($res) $arg = mysql_fetch_array($res);
+        while (!empty($arg['day'])) {
+          $time = $arg['time'];
+          if ($startTime[$i][$ind] <= $time && $endTime[$i][$ind] > $time) return 1;
+          $arg = mysql_fetch_array($res);
+        }
+      }
+      return 0; // I need to check whether my course clash with the selected times
    }
-
    function check ($dayI, $dayJ, $startTimeI, $startTimeJ, $endTimeI, $endTimeJ) {
      if ($dayI != $dayJ) return 0;
      if (($endTimeI < $startTimeJ) || ($endTimeJ < $startTimeI)) return 0;
      return 1; // checks if 2 courses clash or not
    }
   
-
    function clash ($i, $j) {
       global $day, $startTime, $endTime;
       for ($ind1 = 0; $ind1 < count ($day[$i]); ++$ind1) {
@@ -31,50 +40,47 @@
     function solve ($index) {
       global $len, $tables, $ID, $unique_courses, $left, $sz;
      // print ($index);
-   		if ($index > $len) {
+      if ($index > $len) {
       
-   			for ($i = 1; $i <= $len; ++$i) {
-   				for ($j = $i + 1; $j <= $len; ++$j) {
-    				if (clash ($ID[$i], $ID[$j]) == 1) return;
-   				}
-   			}
-   			/*for ($i = 1; $i <= $len; ++$i) {
-   				if (is_collapse ($ID[$i]) == 1) return;
-   			}*/
-   			// if it succeed until this stage then this is good timetable 
-   			array_push($tables, $ID); // all the available timetable ids will be stored in tables
+        for ($i = 1; $i <= $len; ++$i) {
+          for ($j = $i + 1; $j <= $len; ++$j) {
+            if (clash ($ID[$i], $ID[$j]) == 1) return;
+          }
+        }
+        for ($i = 1; $i <= $len; ++$i) {
+          if (is_collapse ($ID[$i]) == 1) return;
+        }
+        // if it succeed until this stage then this is good timetable 
+        array_push($tables, $ID); // all the available timetable ids will be stored in tables
         return ;
-   		}
-   		$course = $unique_courses[$index - 1];
+      }
+      $course = $unique_courses[$index - 1];
     //  print $left[$course]."<br>". $sz[$course];
-   		for ($i = $left[$course]; $i <= $left[$course] + $sz[$course] - 1; ++$i) {
-   			$ID[$index] = $i;// assigning indexes
-   			solve ($index + 1, $len);
-   		}
-
+      for ($i = $left[$course]; $i <= $left[$course] + $sz[$course] - 1; ++$i) {
+        $ID[$index] = $i;// assigning indexes
+        solve ($index + 1, $len);
+      }
    }
-
    include ("bd.php");
    $referenceID = 1; // < --- need to change
    $timetableID = 4; // constant for calculation 
-
    $res = mysql_query("SELECT * FROM `timetable` WHERE `referenceID` = '$referenceID' 
-		                                        AND `timetableID` = '$timetableID'");
-
+                                            AND `timetableID` = '$timetableID'");
   if ($res) $arg = mysql_fetch_array($res);
   $n = 0;
   while(!empty($arg['courseID'])) {
-    	 $CourseID[++$n] = $arg['courseID'];
-   		 $ClassID[$n] = $arg['classID'];  
-   		 $arg = mysql_fetch_array($res);  			
+       $CourseID[++$n] = $arg['courseID'];
+       $ClassID[$n] = $arg['classID'];  
+       $arg = mysql_fetch_array($res);        
   }
+  $courses = array();
   for ($i = 1; $i <= $n; ++$i) {
-  	$classID = $ClassID[$i];
-  	$courseID = $CourseID[$i];
-  	$courses[$i] = $courseID;
-  	$res = mysql_query("SELECT * FROM `classes` WHERE `id` = '$classID' 
-		                                        AND `courseID` = '$courseID'");
-  	if ($res) $arg = mysql_fetch_array($res);
+    $classID = $ClassID[$i];
+    $courseID = $CourseID[$i];
+    $courses[$i] = $courseID;
+    $res = mysql_query("SELECT * FROM `classes` WHERE `id` = '$classID' 
+                                            AND `courseID` = '$courseID'");
+    if ($res) $arg = mysql_fetch_array($res);
       $startTime[$i] = array();
       $endTime[$i] = array();
       $day[$i] = array();
@@ -82,9 +88,9 @@
       $cgroup[$i] = array();
       $ctype[$i] = array();
       $remarks[$i] = array();
-  	while(!empty($arg['courseID'])) {
+    while(!empty($arg['courseID'])) {
       array_push($startTime[$i], $arg['startTime']);
-   	  array_push($endTime[$i], $arg['endTime']);
+      array_push($endTime[$i], $arg['endTime']);
       array_push($day[$i],$arg['day']);
       array_push($venue[$i], $arg['venue']);
       array_push($cgroup[$i], $arg['cgroup']);
@@ -119,13 +125,13 @@
     $left = array();
    // print_r ($unique_courses);
     
- 	for ($i = 1; $i <= $n; ++$i) {
+  for ($i = 1; $i <= $n; ++$i) {
    if (empty($left[$CourseID[$i]])) { // finding left index of specific course ID
- 			$left[$CourseID[$i]] = $i;
- 		}
+      $left[$CourseID[$i]] = $i;
+    }
     if (empty ($sz[$CourseID[$i]])) $sz[$CourseID[$i]] = 1;
- 		else $sz[$CourseID[$i]] ++;
- 	}
+    else $sz[$CourseID[$i]] ++;
+  }
   $tables = array();
   solve (1);
 //
